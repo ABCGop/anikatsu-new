eac0d6f0b18866d71180ec8e77418fa0e68f701e
Vishesh Gangwar <98681783+ABCGop@users.noreply.github.com>
2024-08-09 12:48:56 +0530

    Update _config.php

--- _config.php
+++ _config.php
@@ -22,7 +22,7 @@ $disqus = "https://indianime.disqus.com"; // Disqus
 
 
 // API URL
-$api = ""; //https://github.com/ABCGop/anikatsu-api make your own api
+$api = "https://please-zeta.vercel.app"; //https://github.com/ABCGop/anikatsu-api make your own api
 
 
 $banner = $websiteUrl . "/files/images/banner.png";  //Banner
